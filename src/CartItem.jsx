af467147511efa89fb8f44a267f880f36f7b5a02
Melvin Kamguia <kamguiamelvin@gmail.com>
2026-05-12 16:08:10 -0400

    change

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import { useDispatch } from "react-redux";
+import { updateQuantity, removeItem } from "./CartSlice";
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
@@ -9,27 +11,58 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        let total = 0;
+
+        cart.forEach((item) => {
+            const price = parseFloat(item.cost.substring(1));
+            total += price * item.quantity;
+        });
+
+        return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping(e);
   };
 
 
 
-  const handleIncrement = (item) => {
-  };
+    const handleIncrement = (item) => {
+        dispatch(
+            updateQuantity({
+                name: item.name,
+                quantity: item.quantity + 1
+            })
+        );
+    };
 
-  const handleDecrement = (item) => {
-   
-  };
+    const handleDecrement = (item) => {
+        if (item.quantity > 1) {
+            dispatch(
+                updateQuantity({
+                    name: item.name,
+                    quantity: item.quantity - 1
+                })
+            );
+        } else {
+            dispatch(removeItem(item.name));
+        }
+    };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
+
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
+
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.substring(1));
+    return price * item.quantity;
   };
 
   return (
